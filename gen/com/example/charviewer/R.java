eaa7d6257133021f455d76bab9793925792a667c
reidtbl <reid@twobrightlights.com>
2013-06-29 17:52:34 -0700

    UI changes

--- gen/com/example/charviewer/R.java
+++ gen/com/example/charviewer/R.java
@@ -19,20 +19,26 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int spacer=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int ac_defense=0x7f080000;
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
         public static final int fort_defense=0x7f080001;
+        public static final int hp_current=0x7f080004;
+        public static final int hp_current_temp=0x7f080005;
         public static final int ref_defense=0x7f080002;
+        public static final int surge_number=0x7f080006;
         public static final int will_defense=0x7f080003;
     }
     public static final class layout {
         public static final int attribute_view=0x7f030000;
         public static final int char_viewer=0x7f030001;
+        public static final int def_values=0x7f030002;
+        public static final int hp_values=0x7f030003;
     }
     public static final class menu {
         public static final int char_viewer=0x7f070000;
@@ -40,7 +46,19 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int def_ac=0x7f050003;
+        public static final int def_fort=0x7f050004;
+        public static final int def_ref=0x7f050005;
+        public static final int def_will=0x7f050006;
         public static final int hello_world=0x7f050002;
+        public static final int hp=0x7f050007;
+        public static final int hp_button_damage=0x7f05000a;
+        public static final int hp_button_heal=0x7f05000b;
+        public static final int hp_button_temp=0x7f05000c;
+        public static final int hp_surges=0x7f050008;
+        public static final int hp_surges_expend=0x7f05000e;
+        public static final int hp_surges_regain=0x7f05000d;
+        public static final int hp_temp=0x7f050009;
     }
     public static final class style {
         /** 
@@ -69,7 +87,13 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int attr_button=0x7f060005;
         public static final int attr_label=0x7f060002;
-        public static final int container=0x7f060003;
+        public static final int attr_text_label=0x7f060009;
+        public static final int attr_value=0x7f060003;
+        public static final int container=0x7f060007;
+        public static final int grid_button=0x7f060006;
+        public static final int grid_container=0x7f060008;
+        public static final int partial_value=0x7f060004;
     }
 }
